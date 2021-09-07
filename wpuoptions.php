8396bab90033c497296c2f98962a3975199e9123
Kevin <kevin@rocher-dev.com>
2021-09-07 21:32:35 +0200

    v 5.1.1
    - fix basic WPML check.

--- wpuoptions.php
+++ wpuoptions.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU Options
 Plugin URI: https://github.com/WordPressUtilities/wpuoptions
-Version: 5.1.0
+Version: 5.1.1
 Description: Friendly interface for website options
 Author: Darklg
 Author URI: http://darklg.me/
@@ -17,7 +17,7 @@ class WPUOptions {
 
     private $options = array(
         'plugin_name' => 'WPU Options',
-        'plugin_version' => '5.1.0',
+        'plugin_version' => '5.1.1',
         'plugin_userlevel' => 'manage_categories',
         'plugin_menutype' => 'admin.php',
         'plugin_pageslug' => 'wpuoptions-settings'
@@ -1118,7 +1118,7 @@ function wputh_l18n_get_option($name, $lang = false) {
         if (function_exists('pll_current_language')) {
             $lang = pll_current_language();
         }
-        if (defined('ICL_LANGUAGE_CODE')) {
+        if (!function_exists('pll_current_language') && function_exists('icl_get_languages') && defined('ICL_LANGUAGE_CODE')) {
             $lang = ICL_LANGUAGE_CODE;
         }
     }
@@ -1140,7 +1140,7 @@ function wputh_l18n_get_option($name, $lang = false) {
     if (function_exists('pll_default_language')) {
         $default_language = pll_default_language();
     }
-    if (defined('ICL_LANGUAGE_CODE')) {
+    if (!function_exists('pll_default_language') && function_exists('icl_get_languages') && defined('ICL_LANGUAGE_CODE')) {
         $default_language = apply_filters('wpml_default_language', NULL);
     }
     $default_language = apply_filters('wputh_l18n_get_option__defaultlang', $default_language);
