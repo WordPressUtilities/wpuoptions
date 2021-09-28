a2775078f209cf9f2d4916a33823109b364beb1e
Kevin <kevin@rocher-dev.com>
2021-09-28 17:47:43 +0200

    v 5.2.1
    - version bump.

--- wpuoptions.php
+++ wpuoptions.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: WPU Options
 Plugin URI: https://github.com/WordPressUtilities/wpuoptions
-Version: 5.1.1
+Version: 5.2.1
 Description: Friendly interface for website options
 Author: Darklg
 Author URI: http://darklg.me/
@@ -17,7 +17,7 @@ class WPUOptions {
 
     private $options = array(
         'plugin_name' => 'WPU Options',
-        'plugin_version' => '5.1.1',
+        'plugin_version' => '5.2.1',
         'plugin_userlevel' => 'manage_categories',
         'plugin_menutype' => 'admin.php',
         'plugin_pageslug' => 'wpuoptions-settings'
