dd56491f09a2387148213a9f0705ad0c5f585719
Kevin <kevin@rocher-dev.com>
2023-05-17 15:28:12 +0200

    v 5.4.2
    editor field : fix when original content uses a big image.

--- wpuoptions.php
+++ wpuoptions.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU Options
 Plugin URI: https://github.com/WordPressUtilities/wpuoptions
 Update URI: https://github.com/WordPressUtilities/wpuoptions
-Version: 5.4.1
+Version: 5.4.2
 Description: Friendly interface for website options
 Author: Darklg
 Author URI: http://darklg.me/
@@ -24,7 +24,7 @@ class WPUOptions {
     private $main_url;
     private $options = array(
         'plugin_name' => 'WPU Options',
-        'plugin_version' => '5.4.1',
+        'plugin_version' => '5.4.2',
         'plugin_userlevel' => 'manage_categories',
         'plugin_menutype' => 'admin.php',
         'plugin_pageslug' => 'wpuoptions-settings'
